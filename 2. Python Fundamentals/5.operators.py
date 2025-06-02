bf0824cb431fd8ab7dad335c633f8902ddb981aa
Abhishek <abhishekshettyabhishek250@gmail.com>
2025-06-02 18:01:01 +0530

    commit

--- 2. Python Fundamentals/5.operators.py
+++ 2. Python Fundamentals/5.operators.py
@@ -20,6 +20,7 @@ print("a <= b = ",a<=b)
 
 a = True
 b = False
+
 #Logical operators
 print("a and b = ",a and b)
 print("a or b = ",a or b)
@@ -32,3 +33,23 @@ print("a is not b = ",a is not b)
 #Membership operators
 x = ["apple","banana"]
 print("banana" in x)
+
+#Assignment operators
+a = 3
+a += 3
+print(a)
+a -= 3
+print(a)
+a *= 3
+print(a)
+a /= 3
+print(a)
+a %= 3
+print(a)
+a //= 3
+print(a)
+a **= 3
+print(a)
+a &= 3
+print(a)
+a |= 3
